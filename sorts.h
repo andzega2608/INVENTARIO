8561b2f456af73fa657da5a3c7ba08638c4df91f
andzega2608 <150101221+andzega2608@users.noreply.github.com>
2024-10-01 11:42:39 -0600

    Update sorts.h

--- sorts.h
+++ sorts.h
@@ -1,164 +1,159 @@
 /*
  * Proyecto Inventario de Productos
- * Andrés Ignacio Zegales Taborga
+ * AndrÃ©s Ignacio Zegales Taborga
  * A01711365
- * 31/08/2024
- * Version: 1
+ * 01/10/2024
+ * Version: 2
  *
  * El presente programa posee los metodos de ordenamiento para poder
  * ordenar los productos en base a su precio, venta o disponibilidad.
- * Para ello, este archivo sorts.h posee el metoodo de ordenamiento Merge.
+ * Para ello, este archivo sorts.h posee el metodo de ordenamiento Merge.
  */
 
 #ifndef SORTS_H_
 #define SORTS_H_
-#include <vector>
+
 #include <string>
 #include <sstream>
+#include <vector>
+#include "dlist.h"
 using namespace std;
 
 template <class T>
 class Sorts {
 private:
-    void swap(vector<T>&, int, int);
-    void copyArray(vector<T>&, vector<T>&, int, int);
-    void mergeArray(vector<T>&, vector<T>&, int, int, int);
-    void mergeSplit(vector<T>&, vector<T>&, int, int);
+    void swap(DList<T>&, int, int);
+    void copyDList(DList<T>&, DList<T>&, int, int);
+    void mergeDList(DList<T>&, DList<T>&, int, int, int);
+    void mergeSplit(DList<T>&, DList<T>&, int, int);
 public:
-    vector<T> ordenaMerge(const vector<T>&);
-    vector<string> ordenaProductos(vector<T>&, vector<T>&, vector<string>&);
+    DList<T> ordenaMerge(const DList<T>&);
+    DList<string> ordenaProductos(DList<T>&, DList<T>&, DList<string>&);
 };
 
 /*
-* swap(vector<T> &v, int i, int j)
+* swap(DList<T> &A, int i, int j)
 *
-* Cambia dos valores dentro de un vector
+* Cambia dos valores dentro de un lista doblemente enlazada
 *
-* @param vector<T> &v, int i, int j
+* @param DList<T> &A, int i, int j
 * return
 */
 
 template <class T>
-void Sorts<T>::swap(vector<T> &v, int i, int j) {
-    T aux = v[i];
-    v[i] = v[j];
-    v[j] = aux;
+void Sorts<T>::swap(DList<T> &A, int i, int j) {
+    T aux = A.get(i);
+    A.update(i, A.get(j));
+    A.update(j, aux);
 }
 
 /*
-* copyArray(vector<T> &A, vector<T> &B, int low, int high)
+* copyDList(DList<T> &A, DList<T> &B, int low, int high)
 *
-* Copia el contenido de un array a otro
+* Copia el contenido de una lista a otra
 *
-* @param vector<T> &A, vector<T> &B, int low, int high
+* @param DList<T> &A, DList<T> &B, int low, int high
 * return
 */
 
 template <class T>
-void Sorts<T>::copyArray(vector<T> &A, vector<T> &B, int low, int high) {
+void Sorts<T>::copyDList(DList<T> &A, DList<T> &B, int low, int high) {
     for (int i = low; i <= high; i++) {
-        A[i] = B[i];
+        A.update(i, B.get(i));
     }
 }
 
 /*
-* mergeArray(vector<T> &A, vector<T> &B, int low, int mid, int high)
+* mergeDList(DList<T> &A, DList<T> &B, int low, int mid, int high)
 *
-* Se encarga de ordenar el vector, basicamente hace el procedimiento
-* del metodo Merge
+* Se encarga de ordenar la lista doblemente enlazada, basicamente hace el
+* procedimiento del metodo Merge
 *
-* @param vector<T> &A, vector<T> &B, int low, int mid, int high
+* @param DList<T> &A, DList<T> &B, int low, int mid, int high
 * return
 */
 
 template <class T>
-void Sorts<T>::mergeArray(vector<T> &A, vector<T> &B, int low, int mid, int high) {
-    int i, j, k;
-    i = low;
-    j = mid + 1;
-    k = low;
+void Sorts<T>::mergeDList(DList<T> &A, DList<T> &B, int low, int mid, int high) {
+    int i = low, j = mid + 1, k = low;
     while (i <= mid && j <= high) {
-        if (A[i] > A[j]) {
-            B[k] = A[i];
-            i++;
+        if (A.get(i) > A.get(j)) {
+            B.update(k++, A.get(i++));
         } else {
-            B[k] = A[j];
-            j++;
+            B.update(k++, A.get(j++));
         }
-        k++;
     }
-    if (i > mid) {
-        for (; j <= high; j++) {
-            B[k++] = A[j];
-        }
-    } else {
-        for (; i <= mid; i++) {
-            B[k++] = A[i];
-        }
+    while (i <= mid) {
+        B.update(k++, A.get(i++));
+    }
+    while (j <= high) {
+        B.update(k++, A.get(j++));
     }
 }
 
 /*
-* mergeSplit(vector<T> &A, vector<T> &B, int low, int high)
+* mergeSplit(DList<T> &A, DList<T> &B, int low, int high)
 *
-* Se encarga de dividir el vector en pequeñas partes, para luego
+* Se encarga de dividir la lista en pequeÃ±as partes, para luego
 * hacer el merge, ordenamiento correspondiente
 *
-* @param vector<T> &A, vector<T> &B, int low, int high
+* @param DList<T> &A, DList<T> &B, int low, int high
 * return
 */
 
 template <class T>
-void Sorts<T>::mergeSplit(vector<T> &A, vector<T> &B, int low, int high) {
-    int mid;
-    if ((high - low) < 1) {
+void Sorts<T>::mergeSplit(DList<T> &A, DList<T> &B, int low, int high) {
+    if (high - low < 1) {
         return;
     }
-    mid = (high + low) / 2;
+    int mid = (high + low) / 2;
     mergeSplit(A, B, low, mid);
     mergeSplit(A, B, mid + 1, high);
-    mergeArray(A, B, low, mid, high);
-    copyArray(A, B, low, high);
+    mergeDList(A, B, low, mid, high);
+    copyDList(A, B, low, high);
 }
 
 /*
-* ordenaMerge(const vector<T>& original)
+* ordenaMerge(const DList<T>& original)
 *
 * Integra los dos metodos anteriores, a una manera mucho mas
 * adecuada para el usuario
 *
-* @param const vector<T>& original
-* return v el vector ya ordenado
+* @param const DList<T>& original
+* return DList ya ordenada
 */
 
 template <class T>
-vector<T> Sorts<T>::ordenaMerge(const vector<T>& original) {
-    vector<T> v(original);
-    vector<T> tmp(v.size());
-    mergeSplit(v, tmp, 0, v.size() - 1);
+DList<T> Sorts<T>::ordenaMerge(const DList<T>& original) {
+    DList<T> v(original);
+    DList<T> tmp;
+    for (int i = 0; i < v.size; i++) {
+        tmp.add(T());
+    }
+    mergeSplit(v, tmp, 0, v.size - 1);
     return v;
 }
 
 /*
-* ordenaProductos(vector<T> &tipo_ordenado, vector<T> &tipo, vector<string> &producto)
+* ordenaProductos(DList<T> &tipo_ordenado, DList<T> &tipo, DList<string> &producto)
 *
-* Ordena el vector nombres de los productos en base al vector de precios/ventas/stocks
-* previamente ordenado, basicamente, toma el vector ordenado y el original, con el fin
-* de poder hallar el indice de ese producto y mediante este valor poder ordenar el vector
+* Ordena la DList de los nombres de los productos en base al DList de precios/ventas/stocks
+* previamente ordenado, basicamente, toma la DList ordenado y el original, con el fin
+* de poder hallar el indice de ese producto y mediante este valor poder ordenar el DList
 * de nombres en base a precio/ventas/stocks
 *
-* @param vector<T> &tipo_ordenado, vector<T> &tipo, vector<string> &producto
-* return vector de nombres ordenado acorde a precio/venta/stock
+* @param DList<T> &tipo_ordenado, DList<T> &tipo, DList<string> &producto
+* return DList de nombres ordenado acorde a precio/venta/stock
 */
 
 template <class T>
-vector<string> Sorts<T>::ordenaProductos(vector<T> &tipo_ordenado, vector<T> &tipo, vector<string> &producto) {
-    vector<string> pro_ordenado(tipo_ordenado.size());
-    for (int i = 0; i < tipo_ordenado.size(); i++) {
-        for (int j = 0; j < tipo_ordenado.size(); j++) {
-            if (tipo_ordenado[i] == tipo[j]) {
-                tipo[j] = -1; // Cambia el valor de -1 en caso que existan repetidos
-                pro_ordenado[i] = producto[j];
+DList<string> Sorts<T>::ordenaProductos(DList<T> &tipo_ordenado, DList<T> &tipo, DList<string> &producto) {
+    DList<string> pro_ordenado;
+    for (int i = 0; i < tipo_ordenado.getSize(); i++) {
+        for (int j = 0; j < tipo_ordenado.getSize(); j++) {
+            if (tipo_ordenado.get(i) == tipo.get(j)) {
+                tipo.update(j, -1);
+                pro_ordenado.add(producto.get(j));
                 break;
             }
         }
