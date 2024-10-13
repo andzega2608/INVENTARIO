fd669c8ea816c64d8754ac3a44216bdab2140040
andzega2608 <150101221+andzega2608@users.noreply.github.com>
2024-10-13 13:59:42 -0600

    Update dlist.h

--- dlist.h
+++ dlist.h
@@ -1,8 +1,8 @@
 /*
  * Proyecto Inventario de Productos
- * Andrés Ignacio Zegales Taborga
+ * AndrÃ©s Ignacio Zegales Taborga
  * A01711365
- * 01/10/2024
+ * 13/10/2024
  * Version: 2
  *
  * El presente programa posee los metodos para poder trabajar con estructura de
@@ -14,25 +14,27 @@
 #define DLIST_H_
 
 #include <string>
+#include <vector>
 #include <sstream>
 
-class Inventario; // Se declara la clase Inventario
+class Inventario;
 template <class T> class DList;
 template <class T> class Sorts;
 
 template <class T>
 class DLink {
-private:
-    DLink(T);
-    DLink(T, DLink<T>*, DLink<T>*);
-    DLink(const DLink<T>&);
-    T value;
-    DLink<T> *previous;
-    DLink<T> *next;
-    /*Se declaran clases amigas para que se puedan acceder a los atributos
-    y metodos privados de la clase DLink*/
-    friend class DList<T>;
-    friend class Inventario;
+    private:
+        T value;
+        DLink<T>* previous;
+        DLink<T>* next;
+        /*Se declaran clases amigas para que se puedan acceder a los atributos
+        y metodos privados de la clase DLink*/
+        friend class DList<T>;
+        friend class Inventario;
+    public:
+        DLink(T);
+        DLink(T, DLink<T>*, DLink<T>*);
+        DLink(const DLink<T>&);
 };
 
 // Constructor por default
@@ -41,35 +43,38 @@ DLink<T>::DLink(T val) : value(val), previous(0), next(0) {}
 
 // Constructor por parametros
 template <class T>
-DLink<T>::DLink(T val, DLink *prev, DLink* nxt) : value(val), previous(prev), next(nxt) {}
+DLink<T>::DLink(T val, DLink* prev, DLink* nxt) : value(val), previous(prev), next(nxt) {}
 
 // Constructor por copia
 template <class T>
-DLink<T>::DLink(const DLink<T> &source) : value(source.value), previous(source.previous), next(source.next) {}
+DLink<T>::DLink(const DLink<T>& source) : value(source.value), previous(source.previous), next(source.next) {}
 
 template <class T>
 class DList {
-private:
-    DLink<T> *head;
-    DLink<T> *tail;
-    int size;
-public:
-    DList();
-    DList(const DList<T>&);
-    DList<T>& operator=(const DList<T>&);
-    /*
+    private:
+        DLink<T>* head;
+        DLink<T>* tail;
+        int size;
+    public:
+        DList();
+        DList(const DList<T>&);
+        ~DList();
+        DList<T>& operator=(const DList<T>&);
+        /*
         * getSize()
         * @param
         * return tamano de la lista
-    */
-    int getSize() { return size; }
-    void add(T);
-    int search(T);
-    void update(int, T);
-    void remove(int);
-    T get(int);
-    friend class Sorts<T>;
-    friend class Inventario;
+        */
+        int getSize() const { return size; }
+        void add(T);
+        int search(T) const;
+        void update(int, T);
+        void remove(int);
+        T get(int) const;
+        std::string toString() const;
+        void clear();
+        friend class Sorts<T>;
+        friend class Inventario;
 };
 
 // Constructor por default
@@ -80,10 +85,16 @@ DList<T>::DList() : head(0), tail(0), size(0) {}
 template <class T>
 DList<T>::DList(const DList<T>& original) : head(0), tail(0), size(0) {
     DLink<T>* current = original.head;
-    while (current != 0) {
+    while (current != nullptr) {
         this->add(current->value);
         current = current->next;
     }
+}
+
+// Destructor
+template <class T>
+DList<T>::~DList() {
+    clear();
 }
 
 /*
@@ -121,8 +132,8 @@ DList<T>& DList<T>::operator=(const DList<T>& original) {
 
 template <class T>
 void DList<T>::add(T val) {
-    DLink<T> *p = new DLink<T>(val);
-    if (head == 0 && tail == 0) {
+    DLink<T>* p = new DLink<T>(val);
+    if (head == nullptr && tail == nullptr) {
         head = p;
         tail = p;
     } else {
@@ -144,24 +155,30 @@ void DList<T>::add(T val) {
 */
 
 template <class T>
-int DList<T>::search(T val){
+int DList<T>::search(T val) const {
     int c1 = 0;
     int c2 = size - 1;
     DLink<T> *p = head;
     DLink<T> *q = tail;
-    while (p != q){
-        if (p->value == val){
+    while (p != q && p->next != q) {
+        if (p->value == val) {
             return c1;
+        } else {
+            p = p->next;
+            c1 = c1 + 1;
         }
-        p = p->next;
-        c1 = c1 + 1;
-        if (q->value == val){
+        if (q->value == val) {
             return c2;
+        } else {
+            q = q->previous;
+            c2 = c2 - 1;
         }
-        q = q->previous;
-        c2 = c2 - 1;
     }
-    return -1;
+    if (p->value == val) {
+        return c1;
+    } else {
+        return -1;
+    }
 }
 
 /*
@@ -226,6 +243,7 @@ void DList<T>::remove(int index) {
         } else {
             int c;
             int aux = size / 2;
+
             if (index <= aux) {
                 p = head;
                 c = 0;
@@ -233,8 +251,6 @@ void DList<T>::remove(int index) {
                     p = p->next;
                     c = c + 1;
                 }
-                p->previous->next = p->next;
-                p->next->previous = p->previous;
             } else {
                 p = tail;
                 c = size - 1;
@@ -242,9 +258,9 @@ void DList<T>::remove(int index) {
                     p = p->previous;
                     c = c - 1;
                 }
-                p->next->previous = p->previous;
-                p->previous->next = p->next;
             }
+            p->previous->next = p->next;
+            p->next->previous = p->previous;
             delete p;
         }
     }
@@ -261,7 +277,7 @@ void DList<T>::remove(int index) {
 */
 
 template <class T>
-T DList<T>::get(int index) {
+T DList<T>::get(int index) const {
     DLink<T> *p;
     int c;
     int aux = size / 2;
@@ -277,10 +293,59 @@ T DList<T>::get(int index) {
         c = size - 1;
         while (c != index) {
             p = p->previous;
-            c = c + 1;
+            c = c - 1;
         }
     }
     return p->value;
+}
+
+/*
+* std::string DList<T>::toString() const
+*
+* Retorna un string con los valores de cada nodo denetro
+* de la lista doblemente enlazada
+*
+* param
+* @return string de valores
+*/
+
+template <class T>
+std::string DList<T>::toString() const {
+    std::stringstream aux;
+    DLink<T>* p = head;
+    aux << "[";
+    while (p != nullptr) {
+        aux << p->value;
+        p = p->next;
+        if (p != nullptr) {
+            aux << ", ";
+        }
+    }
+    aux << "]";
+    return aux.str();
+}
+
+/*
+* std::string DList<T>::toString() const
+*
+* Elimina toda la lista doblemente enlazada
+* es un metodo que trabaja con el destructor
+*
+* param
+* @return
+*/
+
+template <class T>
+void DList<T>::clear() {
+    DLink<T>* p = head;
+    while (p != nullptr) {
+        DLink<T>* next = p->next;
+        delete p;
+        p = next;
+    }
+    head = nullptr;
+    tail = nullptr;
+    size = 0;
 }
 
 #endif /* DLIST_H_ */
