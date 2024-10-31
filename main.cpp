ca7b2947907d83c242b3b83f527af5d95a35258c
andzega2608 <150101221+andzega2608@users.noreply.github.com>
2024-10-31 12:43:11 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,18 +2,21 @@
  * Proyecto Inventario de Productos
  * Andrés Ignacio Zegales Taborga
  * A01711365
- * 01/10/2024
- * Version: 2
+ * 31/10/2024
+ * Version: 3
  *
- * El presente programa hace referencia a un algoritmo el cual se encarga de capturar
- * distintos productos y los ordena acorde a su precio, del mas caro al mas barato,
- * segun las unidades que se tienen disponible y segun las ventas que se han generado
- * de cada producto en el mes. Adicional a estas funciones, el proyecto es capaz de
- * añadir productos, eliminar y cambiar algunos datos de cada producto usando una
- * estrcutura de datos lineal como lo son las listas doblemente enlazadas.
- * Adicional a ello, el programa usa AVL para poder estructurar la informacion de ventas
- * y ganancias por dia. Asi tambien mediante esta estrcutura se puede agregar nuevas fechas
- * y realizar una busqueda de una fecha especifica.
+ * El presente programa hace referencia a un algoritmo el cual se
+ * encarga de capturar distintos productos y los ordena acorde a
+ * su precio, del mas caro al mas barato, segun las unidades que
+ * se tienen disponible y segun las ventas que se han generado de
+ * cada producto en el mes. Adicional a estas funciones,
+ * el proyecto es capaz de añadir productos, eliminar y cambiar
+ * algunos datos de cada producto usando una estrcutura de datos
+ * lineal como lo son las listas doblemente enlazadas. Adicional a
+ * ello, el programa usa AVL para poder estructurar la informacion
+ * de ventas y ganancias por dia. Asi tambien mediante esta estrcutura
+ * se puede agregar nuevas fechas y realizar una
+ * busqueda de una fecha especifica.
  */
 
 #include <iostream>
@@ -59,7 +62,8 @@ int main() {
         cin.ignore();
         system("cls");
         if (opcion == 1) {
-            cout << inventario.mostrarDatos(productos, precios, ventas, stocks);
+            cout << inventario.mostrarDatos(productos, precios,
+            ventas, stocks);
         }
         else if (opcion == 2){
             string nombre; int venta, stock; float precio;
@@ -77,28 +81,32 @@ int main() {
         }
         else if (opcion == 3){
             Sorts<float> sorts;
-            cout << "Productos ordenados acorde al precio de manera descendente" << endl;
+            cout << "Productos ordenados acorde al precio" << endl;
             DList<float> pre_ordenado = sorts.ordenaMerge(precios);
-            pro_ordenado = sorts.ordenaProductos(pre_ordenado, aux_precios, productos);
+            pro_ordenado = sorts.ordenaProductos(pre_ordenado,
+            aux_precios, productos);
             cout << inventario.ToString(pro_ordenado, pre_ordenado);
         }
         else if (opcion == 4){
-            cout << "Productos ordenados acorde a las ventas de manera descendente" << endl;
+            cout << "Productos ordenados acorde a las ventas" << endl;
             DList<int> ven_ordenado = sorts.ordenaMerge(ventas);
-            pro_ordenado = sorts.ordenaProductos(ven_ordenado, aux_ventas, productos);
+            pro_ordenado = sorts.ordenaProductos(ven_ordenado,
+            aux_ventas, productos);
             cout << inventario.ToString(pro_ordenado, ven_ordenado);
         }
         else if (opcion == 5){
-            cout << "Productos ordenados acorde al stock de manera descendente" << endl;
+            cout << "Productos ordenados acorde al stock" << endl;
             DList<int> sto_ordenado = sorts.ordenaMerge(stocks);
-            pro_ordenado = sorts.ordenaProductos(sto_ordenado, aux_stocks, productos);
+            pro_ordenado = sorts.ordenaProductos(sto_ordenado,
+            aux_stocks, productos);
             cout << inventario.ToString(pro_ordenado, sto_ordenado);
         }
         else if (opcion == 6){
             string n_producto;
             cout << "Ingresa el nombre del producto a buscar" << endl;
             cout << "Nombre del producto: "; getline(cin, n_producto);
-            cout << inventario.busqueda(productos, precios, ventas, stocks, n_producto);
+            cout << inventario.busqueda(productos, precios, ventas,
+            stocks, n_producto);
             cout << endl;
         }
         else if (opcion == 7){
@@ -108,10 +116,12 @@ int main() {
             int n_pro = productos.search(e_producto); int opcion2;
             cout << endl;
             if (n_pro != -1){
-                cout << "Se eliminara el producto " << productos.get(n_pro) << endl;
+                cout << "Se eliminara el producto "
+                << productos.get(n_pro) << endl;
                 cout << "Ingrese 1 si es correcto: "; cin>>opcion2;
                 if (opcion2 == 1){
-                    inventario.eliminar_product(productos, precios, ventas, stocks, e_producto);
+                    inventario.eliminar_product(productos, precios,
+                    ventas, stocks, e_producto);
                     cout << "Producto eliminado. " << endl;
                 }
                 else {
@@ -119,7 +129,9 @@ int main() {
                 }
             }
             else {
-                cout << "Producto no encontrado. No es posible eliminar un producto inexistente." << endl;
+                cout << "Producto no encontrado. ";
+                cout << "No es posible eliminar un producto inexistente.";
+                cout << endl;
             }
         }
         else if (opcion == 8){
@@ -136,27 +148,34 @@ int main() {
                 if (opcion3 == 1){
                     float precio2;
                     cout << "Ingrese el nuevo precio: "; cin>>precio2;
-                    inventario.actualizar_datos(productos, precios, n_pro2, precio2);
+                    inventario.actualizar_datos(productos, precios,
+                    n_pro2, precio2);
                     cout << "Precio actualizado" << endl;
                 }
                 else if (opcion3 == 2){
                     int stock2;
                     cout << "Ingrese el nuevo stock: "; cin>>stock2;
-                    inventario.actualizar_datos(productos, stocks, n_pro2, stock2);
+                    inventario.actualizar_datos(productos, stocks,
+                    n_pro2, stock2);
                     cout << "Stock actualizado" << endl;
                 }
                 else if (opcion3 == 3){
                     int venta2;
-                    cout << "Ingrese el nuevo numero de ventas: "; cin>>venta2;
-                    inventario.actualizar_datos(productos, ventas, n_pro2, venta2);
+                    cout << "Ingrese el nuevo numero de ventas: ";
+                    cin>>venta2;
+                    inventario.actualizar_datos(productos, ventas,
+                    n_pro2, venta2);
                     cout << "Numero de ventas actualizado" << endl;
                 }
                 else {
-                    cout << "No se ha realizado ninguna actualizacion" << endl;
+                    cout << "No se ha realizado ninguna actualizacion";
+                    cout << endl;
                 }
             }
             else {
-                cout << "Producto no encontrado. No es posible actualizar un producto inexistente." << endl;
+                cout << "Producto no encontrado. ";
+                cout << "No es posible actualizar un producto inexistente.";
+                cout << endl;
             }
         }
         else if (opcion == 9){
@@ -168,7 +187,7 @@ int main() {
             cout << inventario.mostrarFecha() << endl;
         }
         else if (opcion == 11){
-            cout << "Dias ordenados acorde a la ganancia de manera ascendente" << endl;
+            cout << "Dias ordenados acorde a la ganancia" << endl;
             cout << inventario.mostrarFechaVenta() << endl;
         }
         else if (opcion == 12){
@@ -192,7 +211,8 @@ int main() {
             inventario.escribirDatosDia();
             cout << "Muchas Gracias." << endl;
             continua = false;
-            productos.clear(); ventas.clear(); stocks.clear(); precios.clear();
+            productos.clear(); ventas.clear();
+            stocks.clear(); precios.clear();
         }
         else {
             cout << "Opcion invalida. Elige de nuevo." << endl;
